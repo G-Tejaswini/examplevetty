9ce8083b2d16c07bf99c43816cebe5c343018b03
G-Tejaswini <31995127+G-Tejaswini@users.noreply.github.com>
2020-02-20 12:17:09 +0530

    Update BackgroundPage.java

--- BackgroundPage.java
+++ BackgroundPage.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+
 
 public class BackgroundPage {
 
